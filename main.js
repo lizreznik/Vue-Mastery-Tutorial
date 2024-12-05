10cf5d042d2ec63bf575d37fd9e28afb469d8a36
lizreznik <525ereznik@frhsd.com>
2024-12-05 10:40:32 -0500

    Conditional Rendering

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg'
+            image: './assets/images/socks_green.jpg',
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester']
         }
     }
 })
